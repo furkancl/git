22e3a78db89291c4e3b5d4865844965c8ec66632
furkancl <100784768+furkancl@users.noreply.github.com>
2025-07-18 16:12:05 +0300

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+export function formatCurrency(value: number | string, locale = 'tr-TR', currency = 'TRY') {
+  const number = typeof value === 'string' ? parseFloat(value) : value;
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(number);
+}
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
